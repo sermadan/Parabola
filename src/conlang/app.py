a8830d72671b49a6df64a0c8ae0bc2671399428a
SeanFromWP <yuangxuan01@gmail.com>
2026-03-11 11:16:16 +0800

    Added svg icon and removed own project

--- src/conlang/app.py
+++ src/conlang/app.py
@@ -1,21 +1,20 @@
 import os, sys
 from flask import Flask, session
 
-base_path = os.path.dirname(os.path.abspath(__file__))
-if base_path not in sys.path:
-    sys.path.insert(0, base_path)
+current_dir = os.path.dirname(os.path.abspath(__file__))
+src_path = os.path.dirname(current_dir)
+
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
 
 import conlang.paths as paths
-
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "conlanger_secret_key"
 
-    # 確保專案根目錄存在
     os.makedirs(paths.PROJECTS_ROOT, exist_ok=True)
 
-    # 註冊藍圖
     from routes.api import api_bp
     from routes.views import views_bp
     app.register_blueprint(api_bp)
